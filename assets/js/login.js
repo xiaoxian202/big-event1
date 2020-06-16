aa47f42996f0bdaf23a4f38edc819e6bc650823f
byte0 <bbcde_1234@163.com>
2020-06-16 15:22:07 +0800

    实现登录的基本功能

---
+++ assets/js/login.js
@@ -0,0 +1,23 @@
+$(function () {
+
+  // 控制表单的提交
+  $('.layui-form').submit(function (e) {
+    // 阻止表单默认提交行为
+    e.preventDefault()
+    // 获取表单输入域的用户名和密码
+    // username=asdf&password=asdffffff
+    var formData = $(this).serialize()
+    // 调用后台接口验证是否正确
+    $.ajax({
+      type: 'post',
+      url: 'http://ajax.frontend.itheima.net/api/login',
+      data: formData,
+      success: function (res) {
+        // 登录成功后，跳转到主页面
+        if (res.status === 0) {
+          location.href = './index.html'
+        }
+      }
+    })
+  })
+})
