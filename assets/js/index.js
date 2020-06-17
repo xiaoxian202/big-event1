8b4364cbb6943a91b5bbfaf3bcea2d1164efb502
xiaodu <you@example.com>
2020-06-17 10:31:19 +0800

    判断登录成功的标志

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+$(function() {
+    //判断token是否存在
+    var mytoken = localStorage.getItem('mytoken')
+    if(!mytoken) {
+        //mytoken不存在，就跳转登录页面
+        location.href = './login.html'
+    }
+})
