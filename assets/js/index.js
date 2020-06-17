ce253151f50641fc468e117b2e722d9443e12524
xiaodu <you@example.com>
2020-06-17 20:29:47 +0800

    调用接口获取用户信息

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,4 +6,40 @@ $(function() {
         //mytoken不存在，就跳转登录页面
         location.href = './login.html'
     }
+
+    //首页加载时需要调用后台接口获取用户信息
+    function loadUserInfo() {
+        $.ajax({
+            type:'get',
+            url:'http://ajax.frontend.itheima.net/my/userinfo',
+            headers:{
+                // my开头的请求都需要携带请求头，作用：权限验证（只有登录后才能访问）
+                Authorization:localStorage.getItem('mytoken')
+            },
+            success:function(res) {
+                // console.log(res)
+                //获取成功
+                if(res.status === 0) {
+                    // 用户信息
+                    var info = res.data
+                    $('#nav-username').html(info.username)
+                    $('#welcome-username').html(info.username)
+    
+                    //头像
+                    // info.user_pic = 'http://t.cn/RCzsdCq'
+                   if(info.user_pic) {
+                       //删除默认头像
+                       $('#welcome-username').siblings('div').remove()
+                       //添加新头像
+                    //    <img src="http://t.cn/RCzsdCq">
+                       $('#welcome-username').parent().prepend('<img src="'+info.user_pic+'">')
+                   }else {
+                       //显示div
+                   }
+                }
+            }
+    
+        })
+    }
+    loadUserInfo();
 })
