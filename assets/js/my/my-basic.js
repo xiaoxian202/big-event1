7347a21703f1d3868c0e23318fb1cfce9b00b2ec
xiaodu <you@example.com>
2020-06-19 15:10:35 +0800

    更新用户信息

--- assets/js/my/my-basic.js
+++ assets/js/my/my-basic.js
@@ -13,4 +13,28 @@ $(function() {
         })
     }
     loadUserInfo()
+
+    // 控制表单提交
+    $('#form').submit(function(e) {
+        e.preventDefault()
+        //获取表单数据 是一个数组
+        var fd = $(this).serializeArray()
+        // 遍历数组，筛选name: "username"之外的数
+        fd = fd.filter(function(val) {
+            return val.name !== 'username'
+        })
+        // console.log(fd);
+        //调用接口提交用户信息
+        $.ajax({
+            type:'post',
+            url:'my/userinfo',
+            data:fd,
+            success:function(res) {
+                if(res.status === 0) {
+                    //成功获取数据
+                    layer.msg(res.message)
+                }
+            }
+        })
+    })
 })
