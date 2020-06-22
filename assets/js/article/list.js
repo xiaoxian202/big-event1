720d3b4d62216b256593e84398f7b02ea74f43c0
xiaodu <you@example.com>
2020-06-22 10:30:50 +0800

    获取文章列表数据

--- assets/js/article/list.js
+++ assets/js/article/list.js
@@ -1,19 +1,38 @@
 $(function() {
     var form = layui.form
-    function loadUserInfo() {
-            //获取所有文章分类
+    //获取所有文章分类
+    function loadCateData() {
         $.ajax({
             type:'get',
             url:'my/article/cates',
             success:function(res) {
-                console.log(res);
+                // console.log(res);
                 //基于模板引擎渲染分类列表数据
                 var tags = template('cate-tpl',res)
                 $('#category').html(tags)
-                //更新渲染
+                // $('#category').append(tags)
+                //更新渲染 是layui规定
                 form.render('select')
             }
         })
     }
-    loadUserInfo()
+    loadCateData()
+
+    //获取表格列表数据
+    function loadTableData() {
+        $.ajax({
+            type:'get',
+            url:'my/article/list',
+            data:{
+                //页码:必须从1开始
+                pagenum:1,
+                // 每页显示多少条数据
+                pagesize:10
+            },
+            success:function(res) {
+                console.log(res);
+            }
+        })
+    }
+    loadTableData()
 })
